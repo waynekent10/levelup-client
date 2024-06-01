f8aabfbb66b705a94f95e13e09584ea66b0e4475
Bruce Clark <Bruce.clark55@gmail.com>
2024-06-01 00:17:50 -0400

    gameform working

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button } from 'react-bootstrap';
 import GameCard from '../../components/game/GameCard';
 import { getGames } from '../../api/gameData';
 
 function Home() {
   const [games, setGames] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     getGames().then((data) => setGames(data));
@@ -12,9 +15,21 @@ function Home() {
   return (
     <article className="games">
       <h1>Games</h1>
+      <Button
+        onClick={() => {
+          router.push('/games/new');
+        }}
+      >
+        Register New Game
+      </Button>
       {games.map((game) => (
         <section key={`game--${game.id}`} className="game">
-          <GameCard title={game.title} maker={game.maker} numberOfPlayers={game.number_of_players} skillLevel={game.skill_level} />
+          <GameCard
+            title={game.title}
+            maker={game.maker}
+            numberOfPlayers={game.number_of_players}
+            skillLevel={game.skill_level}
+          />
         </section>
       ))}
     </article>
